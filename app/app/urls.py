fb2dc7d8ebe33015a47e5c750e58078283e37ef5
Materia597 <sethj.browning@calbaptist.edu>
2025-11-08 23:27:31 -0800

    Fixed it

--- app/app/urls.py
+++ app/app/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('api/', include('restaurants.urls')),
+    path('api/', include('restuarants.urls')),
     path('admin/', admin.site.urls),
 ]
